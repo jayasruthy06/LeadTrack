6ff1272714a8d09cd78926ca4e6037fb730bb3f3
jayasruthy06 <jayasruthyk6@gmail.com>
2025-07-12 10:42:26 +0530

    Create sample_model.js

---
+++ Backend/sample_model.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const yourschema = new mongoose.Schema({
+  your field:{
+    type: field data type
+  },
+  )}
+
+export default mongoose.model('YourModelName', yourschema);
